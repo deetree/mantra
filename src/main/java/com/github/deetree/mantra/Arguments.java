f9dbb61017fc21e5db6fb08c3526d0551708fa40
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-07-13 15:16:03 +0200

    Add possible options

---
+++ src/main/java/com/github/deetree/mantra/Arguments.java
@@ -0,0 +1,57 @@
+package com.github.deetree.mantra;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+/**
+ * @author Mariusz Bal
+b */
+@Command(name = "mantra", description = "Kickstart your Java (Maven) project",
+        showDefaultValues = true, abbreviateSynopsis = true, version = "Mantra version %s")
+class Arguments {
+
+    @Parameters(description = "Project's name")
+    String name;
+
+    @Option(names = {"--directory", "-d"}, description = "Directory where the project will be created")
+    String directory = System.getProperty("user.home");
+
+    @Option(names = {"--group", "-g"}, description = "Project's groupId")
+    String groupId = "com.example";
+
+    @Option(names = {"--artifact", "-a"}, description = "Project's artifactId")
+    String artifactId;
+
+    @Option(names = {"--java", "-j"}, description = "Java version")
+    int javaVersion = 17;
+
+    @Option(names = {"--no-git", "-n"}, description = "Disable git initialization")
+    boolean disableGit;
+
+    @Option(names = {"--git-username", "-u"}, description = "Set local git username")
+    String gitUsername;
+
+    @Option(names = {"--git-email", "-e"}, description = "Set local git email")
+    String gitEmail;
+
+    @Option(names = {"--help", "-h"}, description = "Show help message", usageHelp = true)
+    boolean helpDemanded;
+
+    @Option(names = {"--version", "-v"}, description = "Show mantra version", versionHelp = true)
+    boolean versionDemanded;
+
+    @Override
+    public String toString() {
+        return "Arguments{" +
+                "name='" + name + '\'' +
+                ", directory='" + directory + '\'' +
+                ", groupId='" + groupId + '\'' +
+                ", artifactId='" + artifactId + '\'' +
+                ", javaVersion=" + javaVersion +
+                ", disableGit=" + disableGit +
+                ", gitUsername='" + gitUsername + '\'' +
+                ", gitEmail='" + gitEmail + '\'' +
+                '}';
+    }
+}
