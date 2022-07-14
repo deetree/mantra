a8abb471e5d8753d59e7c1a7a7a35d309e95cbbc
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-07-14 09:45:39 +0200

    Add helper test

---
+++ src/test/java/com/github/deetree/mantra/HelperTest.java
@@ -0,0 +1,49 @@
+package com.github.deetree.mantra;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.testng.Assert.assertTrue;
+
+@Test
+public class HelperTest {
+
+    private ByteArrayOutputStream output;
+    private final Helper versionHelper = new VersionHelper(new CLIParser(new String[]{"-V"}).parse().cmd());
+    private final Helper usageHelper = new UsageHelper(new CLIParser(new String[]{"-h"}).parse().cmd());
+
+    @BeforeMethod(onlyForGroups = "output")
+    public void setUp() {
+        output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterMethod(onlyForGroups = "output")
+    public void cleanUp() {
+        System.setOut(System.out);
+    }
+
+    public void shouldReturnUsageHelpRequested() {
+        assertTrue(usageHelper.checkHelpRequired());
+    }
+
+    @Test(groups = "output")
+    public void shouldPrintUsageHelp() {
+        usageHelper.printHelp();
+        assertTrue(output.toString().contains("Usage"));
+    }
+
+    public void shouldReturnVersionHelpRequested() {
+        assertTrue(versionHelper.checkHelpRequired());
+    }
+
+    @Test(groups = "output")
+    public void shouldPrintVersionHelp() {
+        versionHelper.printHelp();
+        assertTrue(output.toString().contains("Mantra version"));
+    }
+}
