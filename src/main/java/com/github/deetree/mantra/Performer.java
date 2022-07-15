4ff2a85d2b0f605ab729ce31ea28b95b00d39d3f
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-07-15 15:50:19 +0200

    Add structure creating logic to main worker

--- src/main/java/com/github/deetree/mantra/Performer.java
+++ src/main/java/com/github/deetree/mantra/Performer.java
@@ -2,6 +2,8 @@ package com.github.deetree.mantra;
 
 import picocli.CommandLine;
 
+import java.nio.file.Path;
+
 /**
  * @author Mariusz Bal
  */
@@ -18,12 +20,33 @@ class Performer {
         Helper usage = new UsageHelper(cmd);
         Helper version = new VersionHelper(cmd);
 
-        if (!helpUsed(usage, version)) {
-            System.out.println("Arguments can be processed. No help was demanded.");
+        if (!wasHelpUsed(usage, version)) {
+            Arguments arguments = parsingResult.arguments();
+            Path projectPath = new ProjectPathResolver(arguments.directory, arguments.name).resolve();
+            Path sourcesPath = new SourcePathResolver(projectPath).resolve();
+            Path packagePath = new PackagePathResolver(arguments.groupId, arguments.artifactId).resolve();
+            Path javaMainFilesPath = new JavaFilesPathResolver(sourcesPath, Directory.MAIN, packagePath).resolve();
+            Path mainResourcesPath = new ResourcesPathResolver(sourcesPath, Directory.MAIN).resolve();
+            Path javaTestFilesPath = new JavaFilesPathResolver(sourcesPath, Directory.TEST, packagePath).resolve();
+            Path testResourcesPath = new ResourcesPathResolver(sourcesPath, Directory.TEST).resolve();
+
+            try {
+                createStructure(projectPath, javaMainFilesPath, mainResourcesPath,
+                        javaTestFilesPath, testResourcesPath);
+            } catch (ActionException e) {
+                System.err.println(e.getMessage());
+            }
         }
     }
 
-    private boolean helpUsed(Helper usage, Helper version) {
+    private boolean wasHelpUsed(Helper usage, Helper version) {
         return usage.checkHelpRequired() || version.checkHelpRequired();
+    }
+
+    private void createStructure(Path projectPath, Path javaMainFilesPath, Path mainResourcesPath,
+                                 Path javaTestFilesPath, Path testResourcesPath) {
+        new ProjectDirectoryCreator(projectPath).create();
+        new CodeDirectoryCreator(javaMainFilesPath, mainResourcesPath).create();
+        new CodeDirectoryCreator(javaTestFilesPath, testResourcesPath).create();
     }
 }
