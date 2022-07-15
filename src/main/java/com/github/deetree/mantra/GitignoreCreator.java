812da87207ccac70a3416e070e7ecfa802def5ed
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-07-15 16:10:52 +0200

    Add gitignore creating (copying) class

---
+++ src/main/java/com/github/deetree/mantra/GitignoreCreator.java
@@ -0,0 +1,27 @@
+package com.github.deetree.mantra;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * @author Mariusz Bal
+ */
+class GitignoreCreator implements FileCreator {
+
+    private final Path projectPath;
+
+    GitignoreCreator(Path projectPath) {this.projectPath = projectPath;}
+
+    @Override
+    public void create() {
+        InputStream gitignore = ResourceFileLoader.load(".gitignore");
+        try {
+            Files.copy(gitignore, projectPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            //todo throw new ActionException("An error occurred during .gitignore file creation");
+        }
+    }
+}
