01706a380189c24cfd5cbba234ae80520504b6de
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-07-18 15:03:00 +0200

    Add gitignore creation test

---
+++ src/test/java/com/github/deetree/mantra/GitignoreCreatorIT.java
@@ -0,0 +1,27 @@
+package com.github.deetree.mantra;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Test
+public class GitignoreCreatorIT {
+
+    public void shouldCopyGitignoreToDirectory() throws IOException {
+        //g
+        Path directory = Path.of(System.getProperty("user.home"));
+        Path gitignore = directory.resolve(".gitignore");
+        FileCreator creator = new GitignoreCreator(directory);
+        //w
+        creator.create();
+        //t
+        SoftAssert sa = new SoftAssert();
+        sa.assertTrue(Files.isRegularFile(gitignore), "The .gitignore should be a file");
+        sa.assertTrue(Files.deleteIfExists(gitignore), "The .gitignore file should exist");
+        sa.assertAll();
+    }
+
+}
