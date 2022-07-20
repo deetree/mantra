6983ceba78e9a4553106e1a567925141b38f5409
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-07-20 10:07:10 +0200

    Add operating system detection and shell command executor

---
+++ src/main/java/com/github/deetree/mantra/OperatingSystem.java
@@ -0,0 +1,15 @@
+package com.github.deetree.mantra;
+
+/**
+ * @author Mariusz Bal
+ */
+class OperatingSystem {
+
+    OS identify() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("windows"))
+            return OS.WINDOWS;
+        else //if (os.contains("nux") || os.contains("nix"))
+            return OS.LINUX;
+    }
+}
