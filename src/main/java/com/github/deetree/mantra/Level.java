336166c20495a9ccc02588bf5846b05302fd75c7
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-07-22 14:53:39 +0200

    Add colors to message levels
    This way they are more tightly coupled and can be retrieved without unnecessary, additional methods

--- src/main/java/com/github/deetree/mantra/Level.java
+++ src/main/java/com/github/deetree/mantra/Level.java
@@ -4,8 +4,18 @@ package com.github.deetree.mantra;
  * @author Mariusz Bal
  */
 enum Level {
-    ERROR,
-    WARNING,
-    SUCCESS,
-    INFO
+    ERROR(Color.RED),
+    WARNING(Color.YELLOW),
+    SUCCESS(Color.GREEN),
+    INFO(Color.BLUE);
+
+    private final Color color;
+
+    Level(Color color) {
+        this.color = color;
+    }
+
+    Color color() {
+        return color;
+    }
 }
