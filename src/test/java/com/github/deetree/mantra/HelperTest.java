2dc3effa33fbe9d6eac3243d22198e9af1806f39
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-09-13 15:27:31 +0200

    Optimize imports

--- src/test/java/com/github/deetree/mantra/HelperTest.java
+++ src/test/java/com/github/deetree/mantra/HelperTest.java
@@ -1,11 +1,6 @@
 package com.github.deetree.mantra;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.testng.Assert.assertTrue;
 
