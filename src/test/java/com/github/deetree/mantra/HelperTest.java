d05e0342a04b660d9e1dfc3f9f53551b55432d18
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-09-13 15:26:41 +0200

    Extract output tests to integration tests class

--- src/test/java/com/github/deetree/mantra/HelperTest.java
+++ src/test/java/com/github/deetree/mantra/HelperTest.java
@@ -16,36 +16,12 @@ public class HelperTest {
             new Arguments()).parse());
     private final Helper usageHelper = new UsageHelper(new CLIParser(new String[]{"-h"},
             new Arguments()).parse());
-    private ByteArrayOutputStream output;
-
-    @BeforeMethod(onlyForGroups = "output")
-    public void setUp() {
-        output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
-    }
-
-    @AfterMethod(onlyForGroups = "output")
-    public void cleanUp() {
-        System.setOut(System.out);
-    }
 
     public void shouldReturnUsageHelpRequested() {
         assertTrue(usageHelper.checkHelpRequired());
     }
 
-    @Test(groups = "output")
-    public void shouldPrintUsageHelp() {
-        usageHelper.printHelp();
-        assertTrue(output.toString().contains("Usage"));
-    }
-
     public void shouldReturnVersionHelpRequested() {
         assertTrue(versionHelper.checkHelpRequired());
-    }
-
-    @Test(groups = "output")
-    public void shouldPrintVersionHelp() {
-        versionHelper.printHelp();
-        assertTrue(output.toString().contains("Mantra version"));
     }
 }
