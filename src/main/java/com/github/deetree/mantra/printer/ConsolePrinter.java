20e854ea34d7e9b07074e025dc05d9af414bdd8b
Mariusz Bal <49595495+deetree@users.noreply.github.com>
2022-09-27 10:56:39 +0200

    Add printout suspending

--- src/main/java/com/github/deetree/mantra/printer/ConsolePrinter.java
+++ src/main/java/com/github/deetree/mantra/printer/ConsolePrinter.java
@@ -1,17 +1,44 @@
 package com.github.deetree.mantra.printer;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Queue;
+
 /**
  * @author Mariusz Bal
  */
 class ConsolePrinter implements Printer {
 
+    private boolean printingSuspended;
+    private final Collection<String> suspendedMessages = new ArrayList<>();
+
     @Override
     public void print(Level level, String text) {
-        System.out.printf("[%s%s%s] %s%n", level.color().toString(), level.name(), Color.RESET, text);
+        print("[%s%s%s] %s%n".formatted(level.color().toString(), level.name(), Color.RESET, text));
     }
 
     @Override
     public void print(String text) {
-        System.out.println(text);
+        if (printingSuspended)
+            suspendedMessages.add(text);
+        else
+            printout(text);
+    }
+
+    private void printout(String text) {
+        System.out.print(text);
+    }
+
+    @Override
+    public void suspendPrinting() {
+        printingSuspended = true;
+    }
+
+    @Override
+    public void resumePrinting() {
+        printingSuspended = false;
+        suspendedMessages.forEach(this::printout);
+        suspendedMessages.clear();
     }
 }
